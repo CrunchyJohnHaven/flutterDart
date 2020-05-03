1394fe57e897d1352f6e07b4b1fdec5d5dfaf5cf
CrunchyJohnHaven <john@rettex.com>
2020-05-02 19:54:24 -0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/screens/login_screen.dart';
+import 'package:flutter_login_ui/screens/transition_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login UI',
+      debugShowCheckedModeBanner: false,
+      // home: Page1(),  
+      home: LoginScreen()
+    );
+  }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/screens/transition_screen.dart';
+import 'package:flutter_login_ui/screens/transition_screen2.dart';
+
